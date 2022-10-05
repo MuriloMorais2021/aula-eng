d0d925abc0c0057932f4dae16d317d22ee41b7a2
MuriloMorais2021 <murilo2015morais@gmail.com>
2022-10-05 19:26:01 -0300

    First Commit

---
+++ test.py
@@ -0,0 +1,20 @@
+number = int(10);
+
+print("Hello, Let's go to the game!")
+print("-----------------------------")
+print("type a number from 0 to 10")
+input_number = int(input("type the number here:"))
+print("-----------------------------")
+print("you wrote the number:", input_number)
+print("-----------------------------")
+
+if number == input_number:
+    print('Congratulations! you got it right')
+else:
+    print('OWWWW :(, you have lost the game')
+print('thanks for playing!')
+
+
+
+
+
